9343bf01d4d0cf1511038b9781c1aa9ab7f631d3
Marc Johnson <marc.johnson27591@gmail.com>
2022-12-03 15:48:36 -0500

    [#1] enhance build.sh
    - changes reflect ideas in
      https://sharats.me/posts/shell-script-best-practices/

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-set -euo pipefail
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+    tracing=true
+else
+    tracing=false
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd "$DIR/build"
-go run . $@
+cd "${DIR}/build"
+if [[ "${tracing}" == "true" ]]; then
+    go run . -v "$@"
+else
+    go run . "$@"
+fi
