777772cbf6503a903fb79ee487d3ed22c639f71a
Marc Johnson <marc.johnson27591@gmail.com>
2024-07-21 12:39:34 -0400

    (#2) chore: - update               github.com/majohn-r/tools-build               v0.11.0->v0.12.0             - update golang.org/x/sys v0.21.0               -> v0.22.0 (both build and               library dependencies)             - add deadcode task

--- build/build.go
+++ build/build.go
@@ -25,6 +25,14 @@ var (
 	})
 
 	_ = goyek.Define(goyek.Task{
+		Name:  "deadcode",
+		Usage: "run deadcode analysis",
+		Action: func(a *goyek.A) {
+			build.Deadcode(a)
+		},
+	})
+
+	_ = goyek.Define(goyek.Task{
 		Name:  "doc",
 		Usage: "generate documentation",
 		Action: func(a *goyek.A) {
